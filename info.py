c9dbe5378e39abc20f622daf82300c7e4b7676e2
shareef1232 <shareeftamadaddi70@gmail.com>
2025-03-01 21:37:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ USERNAME = environ.get('USERNAME', "https://telegram.me/Darkanetwork")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002288567117'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+KjHMUk3MPrJjYTI1')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002217495448').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "‎‎mongodb://'darlings:darling@cluster0.vjsbc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI',"mongodb+srv://darlings:darling@cluster0.vjsbc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0‎‎")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
