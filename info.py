b4d7c1daca5086fa4adec422e974998544e98334
shareef1232 <shareeftamadaddi70@gmail.com>
2025-03-27 07:40:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '993294576').split()]
 USERNAME = environ.get('USERNAME', "https://telegram.me/Darkanetwork")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002288567117'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+KjHMUk3MPrJjYTI1')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/moviez_brand')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002217495448').split()]
 DATABASE_URI = environ.get('DATABASE_URI',"mongodb+srv://darlings:darling@cluster0.vjsbc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0‎‎")
 
